2da0fbd79b11bd4d5b429d0f162ad9336b334450
Akankshaunde8686 <akankshaunde4@gmail.com>
2025-07-21 12:32:03 +0530

    Hello

---
+++ Assignment1/Q3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float f,c,k;
+    printf("Enter fahrenherit:");
+    scanf("%f",&f);
+    c=(5.0/9.0)*(f-32);
+    k=c+273.15;
+    printf("celsius:%.2f\n",c);
+    printf("kelvin:%2f\n",k);
+    return 0;
+}
